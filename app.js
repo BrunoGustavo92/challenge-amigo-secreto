cd7015eb5366aec6502ee0edb5278b789c640adc
Bruno Gustavo <bruno-gustavo@outlook.com.br>
2025-08-25 12:00:00 -0300

    Exibindo nomes adicionados

--- app.js
+++ app.js
@@ -13,4 +13,17 @@ function adicionarAmigo() {
 
   amigos.push(nome);
   nomeAmigo.value = '';
+  exibirAmigos();
+}
+
+// Exibindo nomes adicionados
+function exibirAmigos() {
+  const lista = document.getElementById('listaAmigos');
+  lista.innerHTML = '';
+
+  for (let i = 0; i < amigos.length; i++) {
+    const li = document.createElement('li');
+    li.textContent = amigos[i];
+    lista.appendChild(li);
+  }
 }
